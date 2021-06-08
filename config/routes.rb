386c190f81d4eff20264bd447f961e853db2271d
Takahashi-Risa <nakajima.beans@gmail.com>
2021-06-08 15:39:02 +0900

    ユーザー管理機能　修正３

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,5 @@ Rails.application.routes.draw do
   resources :users
   resources :items, only: []do
   end
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
 end
