ee519895429c3158bbc9a3276ae105325ec2b738
Takahashi-Risa <nakajima.beans@gmail.com>
2021-06-09 01:46:06 +0900

    Active Storageの導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,22 @@
 class Item < ApplicationRecord
+  belongs_to :user
+  has_one :records_of_order
+  has_one_attached :image
 
-  
+  with_options presence: true do
+    validates :name
+    validates :info
+    validates :category_id
+    validates :sales_status_id
+    validates :shipping_fee_status_id
+    validates :schedulued_delivery_id
+    validates :price, format: { with: /\A[0-9]+\z/, message: "is invalid"}
+    validates :user
+    validates :prefecture_id
+  end
+
+  private
+
+  def message_params
+    params.require(:item).permit(:name, :image).merge(user_id: cuurent_user.index)
 end
