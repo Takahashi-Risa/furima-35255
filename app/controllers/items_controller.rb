759e93109b8966f6e07c0fd7445e17efea5f4501
Takahashi-Risa <nakajima.beans@gmail.com>
2021-06-14 17:54:34 +0900

    商品出品機能　修正１

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,8 @@
 class ItemsController < ApplicationController
   before_action :move_to_index, except: [:index, :new, :create]
-  before_action :authenticate_user!, except: [:index, :show]
-  before_action :contributor_confirmation, only: [:edit, :update, :destroy]
+  before_action :authenticate_user!, except: [:index]
+  # 後で:showアクションも上記に追加する
+  # before_action :contributor_confirmation, only: [:edit, :update, :destroy]
 
   def index
     @item = Item.includes(:user)
@@ -20,30 +21,30 @@ class ItemsController < ApplicationController
     end
   end
 
-  def show
-  end
+  # def show
+  # end
 
   def edit
   end
 
-  def update
-  end
+  # def update
+  # end
   
-  def destroy
-  end
+  # def destroy
+  # end
 
   private
 
   def item_params
     params.require(:item).permit(:name, :info, :category_id, :sales_status_id, :shipping_fee_status_id, :scheduled_delivery_id,
-                                 :price, :prefecture_id).merge(user_id: current_user.id)
+                                 :price, :prefecture_id, :image).merge(user_id: current_user.id)
   end
 
-  def set_item
-    @item = Item.find(params[:id])
-  end
+  # def set_item
+  #   @item = Item.find(params[:id])
+  # end
 
-  def contributor_confirmation
-    redirect_to root_path unless current_user == @item.user
-  end
+  # def contributor_confirmation
+  #   redirect_to root_path unless current_user == @item.user
+  # end
 end
