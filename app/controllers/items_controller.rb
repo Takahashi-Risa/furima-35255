df58b3d5acca1b257776c525d3c4f7374a036a45
Takahashi-Risa <nakajima.beans@gmail.com>
2021-06-18 20:55:30 +0900

    商品一覧表示３

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,7 +4,7 @@ class ItemsController < ApplicationController
   # before_action :contributor_confirmation, only: [:edit, :update, :destroy]
 
   def index
-    @item = Item.all
+    @items = Item.all
   end
 
   def new
