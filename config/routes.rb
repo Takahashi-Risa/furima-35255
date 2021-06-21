c1a4963b228a4ec5eb4682dd17e89784fd46f42a
Takahashi-Risa <nakajima.beans@gmail.com>
2021-06-21 16:04:13 +0900

    Revert "Revert "Revert "Revert "Revert "destroyアクションの設定"""""
    This reverts commit 6c70a8341367a8f63148861e3dad9f82ca6c195d.

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'items#index'
   resources :users
-  resources :items
+  resources :items, only: [:index, :new, :create, :show, :edit, :update] do
+    # ,:destroyを追記する
+  end
 end
