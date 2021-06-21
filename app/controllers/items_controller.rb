057ef58a2ccc2e776446f2ee1562f7cdcef43c27
Takahashi-Risa <nakajima.beans@gmail.com>
2021-06-21 15:40:21 +0900

    商品情報編集機能　修正３

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,7 +5,7 @@ class ItemsController < ApplicationController
   # :destroyを追加
 
   def index
-    @items = Item.all.includes(:user).order('created_at DESC')
+    @items = Item.all.order('created_at DESC')
   end
 
   def new
