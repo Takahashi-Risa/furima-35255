97d8f7e2a02f6102961f7382d6e0b011429d4d7b
Takahashi-Risa <nakajima.beans@gmail.com>
2021-06-25 14:28:28 +0900

    Update card.js

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -3,6 +3,8 @@ const pay = () => {
   const form = document.getElementById("charge-form")
   form.addEventListener("submit", (e) => {
     e.preventDefault()
+
+    console.log("てっすと");
     
     const formResult = document.getElementById("charge-form")
     const formData = new FormData(formResult)
